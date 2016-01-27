3331cd0d65e44ed72370511a33b9043ba5d37c4d
christophergandrud <christopher.gandrud@gmail.com>
2016-01-27 15:00:38 +0000

    re-work order, add to surveys

--- seminars/week_4/refugee_word_cloud.R
+++ seminars/week_4/refugee_word_cloud.R
@@ -18,6 +18,7 @@ setwd('/git_repositories/city_sg1022/seminars/week_4/')
 pal <- brewer.pal(8,"Set2")
 
 # September 2015
+## http://www.bundeskanzlerin.de/Content/EN/Artikel/2015/09_en/2015-09-09-generaldebatte-merkel_en.html
 clean_sept <- Corpus(DirSource('refugees_pre/')) %>%
     tm_map(stripWhitespace) %>%
     tm_map(removePunctuation) %>%
@@ -29,6 +30,8 @@ clean_sept <- Corpus(DirSource('refugees_pre/')) %>%
 wordcloud(clean_sept, scale=c(8, 0.2),
           random.order = FALSE, rot.per = .15, colors = pal2)
 
+# January 2016
+# http://www.bundeskanzlerin.de/Content/EN/Artikel/2016/01_en/2016-01-11-konsequenzen-nach-koeln_en.html
 clean_jan <- Corpus(DirSource('refugees_post/')) %>%
     tm_map(stripWhitespace) %>%
     tm_map(removePunctuation) %>%
