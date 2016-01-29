9324e5245e4dd33e19c45da1d88e1456809086e2
christophergandrud <christopher.gandrud@gmail.com>
2016-01-29 08:21:06 +0000

    seminar 1 ready

---
+++ seminars/week_1/examples/simple_source.R
@@ -0,0 +1,6 @@
+# Simple R source code file illustration ------------------------------------- #-
+
+# Create Fake data frame --------
+fake_data <- data.frame(countries = c('Algeria', 'Cambodia', 'Zambia'),
+                        continent = c('Africa', 'Asia', 'Africa'),
+                        population_million = c(39, 15.3, 15.7))
