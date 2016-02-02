5beb0a0b9325ac4dd51469a002805179095d1f24
christophergandrud <christopher.gandrud@gmail.com>
2016-02-02 07:49:35 +0100

    composite indicators lecture started

---
+++ lectures/composite_indicators/plots/composite_plot_maker.R
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------- #
+# Plots for composite plots lecture
+# Christopher Gandrud
+# MIT LICENSE
+# ---------------------------------------------------------------------------- #
+
+library(rio)
+library(dplyr)
+library(repmis)
+library(ggplot2)
+
+# Set working directory
+possibles <- c('/git_repositories/city_sg1022/lectures/composite_indicators/')
+
+set_valid_wd(possibles)
+
