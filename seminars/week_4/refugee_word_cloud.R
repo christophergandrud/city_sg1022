361a230d78bd5dbe7e61f99f7188c4b23145adac
christophergandrud <christopher.gandrud@gmail.com>
2016-02-08 17:22:34 +0100

    content analysis lecture start

--- seminars/week_4/refugee_word_cloud.R
+++ seminars/week_4/refugee_word_cloud.R
@@ -15,7 +15,7 @@ library(RColorBrewer)
 setwd('/git_repositories/city_sg1022/seminars/week_4/')
 
 # Set colour palette
-pal <- brewer.pal(8,"Set2")
+pal <- brewer.pal(8, "Set2")
 
 # September 2015
 ## http://www.bundeskanzlerin.de/Content/EN/Artikel/2015/09_en/2015-09-09-generaldebatte-merkel_en.html
@@ -28,7 +28,7 @@ clean_sept <- Corpus(DirSource('refugees_pre/')) %>%
     tm_map(removeWords, words = c('angela', 'merkel'))
 
 wordcloud(clean_sept, scale=c(8, 0.2),
-          random.order = FALSE, rot.per = .15, colors = pal2)
+          random.order = FALSE, rot.per = .15, colors = pal)
 
 # January 2016
 # http://www.bundeskanzlerin.de/Content/EN/Artikel/2016/01_en/2016-01-11-konsequenzen-nach-koeln_en.html
