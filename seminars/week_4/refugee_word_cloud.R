4a95941fe0171cf3c8a4d2a0309092018d1ae19d
christophergandrud <christopher.gandrud@gmail.com>
2016-02-10 09:46:53 +0100

    text analysis seminar 1st draft

--- seminars/week_4/refugee_word_cloud.R
+++ seminars/week_4/refugee_word_cloud.R
@@ -11,7 +11,7 @@ library(tm)
 library(wordcloud)
 library(RColorBrewer)
 
-# Set working directory
+# Set working directory. Change as needed.
 setwd('/git_repositories/city_sg1022/seminars/week_4/')
 
 # Set colour palette
@@ -19,27 +19,32 @@ pal <- brewer.pal(8, "Set2")
 
 # September 2015
 ## http://www.bundeskanzlerin.de/Content/EN/Artikel/2015/09_en/2015-09-09-generaldebatte-merkel_en.html
-clean_sept <- Corpus(DirSource('refugees_pre/')) %>%
+pre <- Corpus(DirSource('refugees_pre/')) %>%
     tm_map(content_transformer(tolower)) %>%
     tm_map(stripWhitespace) %>%
     tm_map(removePunctuation) %>%
     tm_map(removeNumbers) %>%
     tm_map(removeWords,
-           stopwords(kind = "SMART")) %>%
-    tm_map(removeWords, words = c('angela', 'merkel'))
+           stopwords(kind = 'english')) %>%
+    tm_map(stemDocument) %>%
+    tm_map(removeWords,
+            words = c('angela', 'merkel'))
 
-wordcloud(clean_sept, scale=c(8, 0.2),
+wordcloud(pre, scale=c(8, 0.2),
           random.order = FALSE, rot.per = .15, colors = pal)
 
 # January 2016
 # http://www.bundeskanzlerin.de/Content/EN/Artikel/2016/01_en/2016-01-11-konsequenzen-nach-koeln_en.html
-clean_jan <- Corpus(DirSource('refugees_post/')) %>%
+post <- Corpus(DirSource('refugees_post/')) %>%
     tm_map(content_transformer(tolower)) %>%
     tm_map(stripWhitespace) %>%
     tm_map(removePunctuation) %>%
     tm_map(removeNumbers) %>%
     tm_map(removeWords,
-           stopwords(kind = "SMART"))
+           stopwords(kind = 'english')) %>%
+    tm_map(stemDocument) %>%
+    tm_map(removeWords,
+            words = c('angela', 'merkel'))
 
-wordcloud(clean_jan, scale=c(8, 0.2),
+wordcloud(post, scale=c(8, 0.2),
           random.order = FALSE, rot.per = 0.15, colors = pal)
